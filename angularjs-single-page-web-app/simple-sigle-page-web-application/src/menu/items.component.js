17137473a27fb7d576a01cb5b5787694fbaf2e36
Yogesh Choudhary <youzendachoudhary22@gmail.com>
2020-08-01 18:21:31 +0530

    added the simple single page application

---
+++ angularjs-single-page-web-app/simple-sigle-page-web-application/src/menu/items.component.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular.module('MenuApp')
+  .component('items', {
+    templateUrl: 'src/menu/templates/items.template.html',
+    bindings: {
+      items: '<'
+    }
+  });
+})();
